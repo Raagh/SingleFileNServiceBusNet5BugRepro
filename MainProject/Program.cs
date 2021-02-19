cc1cd3797efdee136e3b24064c4d79651ecbafd6
Patricio Ferraggi <pattferraggi@gmail.com>
2021-02-19 09:40:53 +0100

    bug reproduction

---
+++ MainProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace MainProject
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            EndpointConfiguration configuration = new EndpointConfiguration("MainProject");
+
+            configuration.UseTransport<LearningTransport>();
+            configuration.UsePersistence<LearningPersistence>();
+            
+            IEndpointInstance endpoint = await NServiceBus.Endpoint.Start(configuration).ConfigureAwait(false);
+
+            Console.WriteLine("Main project is running");
+            Console.WriteLine("Press Enter to stop it...");
+            Console.ReadLine();
+
+            await endpoint.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
